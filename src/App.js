89fa03c4d9873f9748eac5dd47c4ba7f544ed83b
Tuong Vi Pham <tvpham11@gmail.com>
2019-01-25 15:05:25 -0500

    changed api url to call just current weather, rather than forecast, for ease of following along tutorial; once app is working, will go in and make changes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch  (`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
+    const api_call = await fetch  (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
 
     const weatherData = await api_call.json();
     console.log(weatherData);
