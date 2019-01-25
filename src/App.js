d2d3a71b5103f8ad79f33e7e586c76f5f7982d7f
Tuong Vi Pham <tvpham11@gmail.com>
2019-01-25 16:50:17 -0500

    added in error message that shows up when no city and country are entered

--- src/App.js
+++ src/App.js
@@ -25,17 +25,28 @@ class App extends React.Component {
     const api_call = await fetch  (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
 
     const weatherData = await api_call.json();
-    console.log(weatherData);
-
-    this.setState({
-      city: weatherData.name,
-      country: weatherData.sys.country,
-      temperature: weatherData.main.temp,
-      humidity: weatherData.main.humidity,
-      description: weatherData.weather[0].description,
-      error: undefined
-    });
     
+    if (city && country) {
+      console.log(weatherData);
+
+      this.setState({
+        city: weatherData.name,
+        country: weatherData.sys.country,
+        temperature: weatherData.main.temp,
+        humidity: weatherData.main.humidity,
+        description: weatherData.weather[0].description,
+        error: ""
+      })
+    } else {
+      this.setState({
+        city: undefined,
+        country: undefined,
+        temperature: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the city and country.'
+      })
+    }
   }
 
   render() {
@@ -49,6 +60,7 @@ class App extends React.Component {
           temperature = {this.state.temperature}
           humidity = {this.state.humidity}
           description = {this.state.description}
+          error = {this.state.error}
         />
       </div>
     );
