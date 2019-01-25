78ebc01c926d7ce59e8408402743acc3c8e72922
Tuong Vi Pham <tvpham11@gmail.com>
2019-01-25 15:01:32 -0500

    built out title, form, and weather components as well as set up api key and url to be called onsubmit of form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '2a0f33fbf7b1f8aa1b6d8e746d3eff7f';
+
+class App extends React.Component {
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch  (`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
+
+    const weatherData = await api_call.json();
+    console.log(weatherData);
+    
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Title />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
